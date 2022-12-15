d22e35f32dc23268bed431a4801a7c72a0bde026
Sarah Sanborn <sanbornsarahn@gmail.com>
2022-12-15 14:01:48 -0800

    Updates made to begin lifting state. Not yet complete.

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
 
@@ -16,13 +16,35 @@ const TASKS = [
 ];
 
 const App = () => {
+  const initialCopy = TASKS.map((task) => {
+    return {...task};
+  });
+
+  const [taskList, setTaskList] = useState(initialCopy);
+
+  const updateComplete = (taskId, updateComplete) => {
+    const newTaskList = [];
+    for (const task of taskList) {
+      if (task.id !== taskId) {
+        newTaskList.push(task);
+      } else {
+        const newTask = {
+          ...task,
+          isComplete: updateComplete
+        };
+        newTaskList.push(newTask);
+      }
+    }
+    setTaskList(newTaskList);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>{<TaskList tasks={TASKS} updateComplete={updateComplete} />}</div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,17 +1,29 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
-
 import './Task.css';
 
-const Task = ({ id, title, isComplete }) => {
-  const [complete, setComplete] = useState(isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+const Task = ({ id, title, isComplete, updateComplete }) => {
+  // const [complete, setComplete] = useState(isComplete);
+
+
+  // const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+
+
+  // THIS IS WHERE WE GOT LOST
+  const updateCompleteButtonClick = () => {
+    const updateCompleteButton = {
+      id: {id},
+      titleData: {title},
+      isComplete: (isComplete ? 'tasks__item__toggle--completed' : '')
+    };
+    updateComplete(id, updateCompleteButtonClick); // SHOULD THIS BE A FUNCTION OR A VARIABLE?!?!?!!
+  };
 
   return (
     <li className="tasks__item">
       <button
-        className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        className={`tasks__item__toggle ${updateCompleteButtonClick}`} // SAME QUESTION HERE
+        onClick={() => updateComplete(!isComplete)}
       >
         {title}
       </button>
@@ -24,6 +36,7 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  updateComplete: PropTypes.func.isRequired
 };
 
 export default Task;
