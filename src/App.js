30eb1df8ac00efafb328a86bebd20e79941c4cce
Carmen Vega <cvega23@horizon.csueastbay.edu>
2022-12-19 11:59:36 -0800

    Started Wave 3, completed connection front end to back end and deleteTask works, updateComplete does not work yet

--- src/App.js
+++ src/App.js
@@ -1,53 +1,86 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import axios from 'axios';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
-  const initialCopy = TASKS.map((task) => {
-    return { ...task };
-  });
+  // const initialCopy = TASKS.map((task) => {
+  //   return { ...task };
+  // });
 
-  const [taskList, setTaskList] = useState(initialCopy);
+  const [taskList, setTaskList] = useState([]);
+  const URL = 'http://127.0.0.1:5000/tasks';
+  useEffect(() => {
+    axios
+      .get(URL)
+      .then((response) => {
+        // console.log(response);
+        const tasksApiResCopy = response.data.map((task) => {
+          return {
+            ...task,
+          };
+        });
+        setTaskList(tasksApiResCopy);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
+  // Tasks not updating on website
   const updateComplete = (taskId, isComplete) => {
     const newTaskList = [];
-    for (const task of taskList) {
-      if (task.id !== taskId) {
-        newTaskList.push(task);
-      } else {
-        const newTask = {
-          ...task,
-          isComplete: !isComplete,
-        };
-        newTaskList.push(newTask);
-      }
-    }
-    setTaskList(newTaskList);
-    console.log("We're in the updateComplete function");
-    console.log(newTaskList);
+    axios
+      .patch(`${URL}/${taskId}/${isComplete}`)
+      .then((response) => {
+        for (const task of taskList) {
+          if (task.id !== taskId) {
+            newTaskList.push(task);
+          } else {
+            const newTask = {
+              ...task,
+              isComplete: !isComplete,
+            };
+            newTaskList.push(newTask);
+          }
+        }
+        setTaskList(newTaskList);
+        console.log("We're in the updateComplete function");
+        console.log(newTaskList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   const deleteTask = (taskId) => {
-    const newTaskList = [];
-    for (const task of taskList) {
-      if (task.id !== taskId) {
-        newTaskList.push(task);
-      }
-    }
-    setTaskList(newTaskList);
+    axios
+      .delete(`${URL}/${taskId}`)
+      .then(() => {
+        const newTaskList = [];
+        for (const task of taskList) {
+          if (task.id !== taskId) {
+            newTaskList.push(task);
+          }
+        }
+        setTaskList(newTaskList);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   };
 
   return (
